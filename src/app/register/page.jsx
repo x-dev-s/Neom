16326c5c00b4bf5174d877ae2d47db62e5800bd0
Abdullah Sultan <abdullahsultan546@gmail.com>
2025-03-17 17:31:04 +0500

    auth added

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,21 +1,21 @@
 import Register from '@/components/UI/Register/main';
 
 export const metadata = {
-    title: "Login - " + process.env.WEBSITE_NAME,
-    description: "Login to access the web portal for live monitoring of 937kWp PV Diesel Hybrid System",
+    title: "Register - " + process.env.WEBSITE_NAME,
+    description: "Register to access the web portal for live monitoring of 937kWp PV Diesel Hybrid System",
     image: "/images/logo.png",
     openGraph: {
-        title: "Login - " + process.env.WEBSITE_NAME,
-        description: "Login to access the web portal for live monitoring of 937kWp PV Diesel Hybrid System",
+        title: "Register - " + process.env.WEBSITE_NAME,
+        description: "Register to access the web portal for live monitoring of 937kWp PV Diesel Hybrid System",
         image: "/images/logo.png",
     },
     twitter: {
-        title: "Login - " + process.env.WEBSITE_NAME,
-        description: "Login to access the web portal for live monitoring of 937kWp PV Diesel Hybrid System",
+        title: "Register - " + process.env.WEBSITE_NAME,
+        description: "Register to access the web portal for live monitoring of 937kWp PV Diesel Hybrid System",
         image: "/images/logo.png",
     },
 };
 
-export default function LoginPage() {
+export default function RegisterPage() {
     return <Register />;
 }
